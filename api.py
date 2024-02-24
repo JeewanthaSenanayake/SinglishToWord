82bca42979a19f23e77d0e14118eb5f6ccb38821
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-02-25 01:04:33 +0530

    SinglishToWord

---
+++ api.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import pyautogui
+from fastapi.middleware.cors import CORSMiddleware
+import keyboard
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/write_on_doc/")
+def write_on_doc(data: dict):
+    msg = data['text']
+    print(msg)
+    # pyautogui.write(f'{msg}')
+    keyboard.write(f'{msg}')
+    
+    return {"msg": "done"}
+
+# pip install pyinstaller
+# pyinstaller --onefile main.py
---
+++ main.py
@@ -0,0 +1,18 @@
+import webbrowser
+import uvicorn
+import pyautogui
+from api import app  
+
+def run_server():
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+    
+def open_html_file(html_file_path):
+    # Open the HTML file in the default web browser
+    webbrowser.open(html_file_path, new=2)
+
+if __name__ == "__main__":
+    html_file_path = "index.html"
+    open_html_file(html_file_path)
+    run_server()
+
+
